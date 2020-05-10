16f5cd848959393741c6283a2f935a9578e7cdbd
perronemirko <mko.prr@gmail.com>
2020-05-10 11:36:33 +0200

    added front-end-test cord origin dev and prod

--- src/main.ts
+++ src/main.ts
@@ -9,24 +9,40 @@ import { Logger } from '@nestjs/common';
 import * as config from 'config'; // in the root folder not src
 
 async function bootstrap() {
-  
+  var cors = require('cors');
   const logger = new Logger("bootstrap");
   
-  const privateKey = fs.readFileSync('./src/ssl/private.key', 'utf8');
-  const certificate = fs.readFileSync('./src/ssl/certificate.crt', 'utf8');
-  const httpsOptions = {key: privateKey, cert: certificate};
+  let privateKey = fs.readFileSync('./src/ssl/private.key', 'utf8');
+  let certificate = fs.readFileSync('./src/ssl/certificate.crt', 'utf8');
+  let httpsOptions = {key: privateKey, cert: certificate};
   
   const server = express();
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(server),
   );
-  if (process.env.NODE_ENV === 'development'){
-    app.enableCors();
-  }
-  await app.init();
 
-  //http.createServer(server).listen(80);
+  
+
+  if (process.env.NODE_ENV === 'development'){
+    logger.log("development")
+    privateKey = fs.readFileSync('./src/ssl/test.key', 'utf8');
+    certificate = fs.readFileSync('./src/ssl/test.crt', 'utf8');
+    httpsOptions = {key: privateKey, cert: certificate};
+    // logger.log("privateKey => " + privateKey)
+    // logger.log("certificate => " + certificate)
+    // logger.log("httpsOptions key => " + httpsOptions.key)
+    // logger.log("httpsOptions cert => " + httpsOptions.cert)
+    app.enableCors();
+    await app.init();
+    https.createServer(httpsOptions, server).listen(3000);
+  }
+  logger.log("production")
+  app.use(cors());
+  app.enableCors();
+  await app.init();
+  
+  http.createServer(server).listen(4300);
   https.createServer(httpsOptions, server).listen(3000);
 }
 
